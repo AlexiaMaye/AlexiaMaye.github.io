111e0e844f7cb17418a520e5433846af02c6940f
AlexiaMaye <krazykitty3.14@gmail.com>
2017-07-09 16:24:22 -0700

    Update and rename index.html to index.php

--- chat/index.php
+++ chat/index.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Chat</title>
+<title>Chat - Customer Module</title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
-<body>
-
+ 
 <div id="wrapper">
     <div id="menu">
         <p class="welcome">Welcome, <b></b></p>
@@ -26,8 +25,10 @@
 $(document).ready(function(){
  
 });
-</script>    
-    <?
+</script>
+</body>
+</html>
+<?
 session_start();
  
 function loginForm(){
@@ -52,7 +53,7 @@ if(isset($_POST['enter'])){
     }
 }
 ?>
-    <?php
+<?php
 if(!isset($_SESSION['name'])){
     loginForm();
 }
@@ -80,6 +81,31 @@ $(document).ready(function(){
 <?php
 }
 ?>
-    <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
-</body>
-</html>
+<p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+	//If user wants to end session
+	$("#exit").click(function(){
+		var exit = confirm("Are you sure you want to end the session?");
+		if(exit==true){window.location = 'index.php?logout=true';}		
+	});
+});
+    if(isset($_GET['logout'])){ 
+     
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+    $("#submitmsg").click(function(){	
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});				
+		$("#usermsg").attr("value", "");
+		return false;
+	});
+</script>
+

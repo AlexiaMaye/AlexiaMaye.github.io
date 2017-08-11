29196072fa6f6f6052156774970bb12f74ad3936
AlexiaMaye <krazykitty3.14@gmail.com>
2017-08-10 19:24:42 -0700

    Update script.js

--- scratchpaper/script.js
+++ scratchpaper/script.js
@@ -1,4 +1,4 @@
-var scrollMonitor = require("./scrollMonitor"); // if you're not using require, you can use the scrollMonitor global.
+
 var myElement = document.getElementById("itemToWatch");
 
 var elementWatcher = scrollMonitor.create( myElement );
